335425f855164cc782220d3d8a75c67c3a2ae7f1
command <jawad.grar@unity3d.com>
2023-12-16 18:39:11 -0500

    start base game

---
+++ Assets/Scripts/TouchPoint.cs
@@ -0,0 +1,45 @@
+// 2023-12-13 AI-Tag 
+// This was created with assistance from Muse, a Unity Artificial Intelligence product
+using UnityEngine;
+
+public class TouchPoint : MonoBehaviour
+{
+    public GameObject circlePrefab;
+
+    void Start()
+    {
+        circlePrefab.SetActive(false);
+    }
+
+    void Update()
+    {
+        // add mobile touch support
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            if (touch.phase == TouchPhase.Began || touch.phase == TouchPhase.Moved)
+            {
+                Vector2 touchPosition = Camera.main.ScreenToWorldPoint(touch.position);
+                circlePrefab.SetActive(true);
+                circlePrefab.transform.position = touchPosition;
+            }
+            else if (touch.phase == TouchPhase.Ended)
+            {
+                circlePrefab.SetActive(false);
+            }
+        }
+        // add mouse support
+        else if (Input.GetMouseButtonDown(0))
+        {
+            Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                circlePrefab.SetActive(true);
+            circlePrefab.transform.position = mousePosition;
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            circlePrefab.SetActive(false);
+        }
+
+    }
+}
